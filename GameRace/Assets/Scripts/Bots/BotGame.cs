8fbe8471157564a776450f1e6894f56897329855
FotonMoonLight <datahardofficial@gmail.com>
2023-03-04 20:12:55 +0300

    Alfa 0.03
    -Добавлен Bot
    -Добавлена карта

---
+++ GameRace/Assets/Scripts/Bots/BotGame.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotGame : MonoBehaviour
+{
+    public float speedBot;
+	public GameObject _Point;
+	public int _TurnCounter = 0;
+	public int _AngleCount = 0;
+	private bool _HasTurn = true;
+    void FixedUpdate()
+    {
+		Vector3 vb = new Vector3(_Point.transform.position.x - transform.position.x, _Point.transform.position.y - transform.position.y, _Point.transform.position.z - transform.position.z);
+		transform.position = transform.position + speedBot * vb.normalized * Time.fixedDeltaTime;
+		TurnController();
+    }
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.tag == "BotRight")
+		{
+			_TurnCounter = 1;
+		}
+		if (collision.gameObject.tag == "BotLeft")
+		{
+			_TurnCounter = 2;
+		}
+	}
+	private void TurnController()
+	{
+		if(_TurnCounter == 1)
+		{
+			if (_HasTurn && _AngleCount < 90)
+			{
+				_HasTurn = false;
+				StartCoroutine(turnTimerRight());
+			}
+			if(_AngleCount == 90)
+			{
+				_TurnCounter = 0;
+				_HasTurn = true;
+				_AngleCount = 0;
+			}
+		}
+		if (_TurnCounter == 2)
+		{
+			if (_HasTurn && _AngleCount < 90)
+			{
+				_HasTurn = false;
+				StartCoroutine(turnTimerLeft());
+			}
+			if (_AngleCount == 90)
+			{
+				_TurnCounter = 0;
+				_HasTurn = true;
+				_AngleCount = 0;
+			}
+		}
+	}
+	IEnumerator turnTimerRight()
+	{
+		yield return new WaitForSeconds(0.001f);
+		transform.Rotate(0, 0, -3f);
+		_AngleCount += 3;
+		_HasTurn = true;
+	}
+	IEnumerator turnTimerLeft()
+	{
+		yield return new WaitForSeconds(0.001f);
+		transform.Rotate(0, 0, 3f);
+		_AngleCount += 3;
+		_HasTurn = true;
+	}
+}
--- GameRace/Assets/Scripts/PlayerControl.cs
+++ GameRace/Assets/Scripts/PlayerControl.cs
@@ -7,16 +7,18 @@ public class PlayerControl : MonoBehaviour
     public int _CouterSprites = 0;
     public int _CountAngle = 0;
 
-    public float _MovementSpeed = 0;
+    public float _MovementSpeed = 0.4f;
+    private float move;
 
     public bool _HasSpeedUp = true;
 
     public GameObject PlayerMask;
     public GameObject dot;
+
  
     void Start()
     {
-        
+      
     }
 
 
@@ -24,48 +26,28 @@ public class PlayerControl : MonoBehaviour
     {
         PlayerMove();
     }
-	private void Update()
-	{
-        if (Input.GetKey(KeyCode.W))
-        {
-            _HasSpeedUp = true;
-            if (_MovementSpeed < 2f && _HasSpeedUp)
-            {
-                _HasSpeedUp = false;
-                StartCoroutine(SpeedPlus());
-            }
-
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            _MovementSpeed = 0;
-        }
-    }
+	
 	void PlayerMove()
 	{
+        move = Input.GetAxis("Vertical");
         Vector3 vb = new Vector3(dot.transform.position.x - transform.position.x, dot.transform.position.y - transform.position.y, dot.transform.position.z - transform.position.z);
 		if (Input.GetKey(KeyCode.W))
 		{
-            transform.position = transform.position + _MovementSpeed * vb.normalized * Time.fixedDeltaTime;
+            transform.position = transform.position + _MovementSpeed * vb.normalized * move * Time.fixedDeltaTime;
         }
 		if (Input.GetKey(KeyCode.A))
 		{
-            transform.Rotate(0, 0, 5f);
+            transform.Rotate(0, 0, 3f);
 		}
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0, 0, -5f);
+            transform.Rotate(0, 0, -3f);
         }
 		if (Input.GetKey(KeyCode.S))
 		{
-            transform.position = transform.position + 2f * -vb.normalized * Time.fixedDeltaTime;
+            transform.position = transform.position + 3f * move * vb.normalized * Time.fixedDeltaTime;
         }
 		
 
     }
-    IEnumerator SpeedPlus()
-	{
-        yield return new WaitForSeconds(0.8f);
-        _MovementSpeed += 0.01f;
-	}
 }
